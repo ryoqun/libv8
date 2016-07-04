9b20a1817036b95fe6b891fb823fb4ed9b735fa7
Petko Bordjukov <bordjukov@gmail.com>
2016-07-04 15:08:54 +0300

    Move up LCC in the detection list

--- ext/libv8/compiler.rb
+++ ext/libv8/compiler.rb
@@ -13,8 +13,8 @@ module Libv8
       case version_string_of(compiler)
       when /^Apple LLVM\b/ then AppleLLVM
       when /\bclang\b/i then Clang
-      when /^gcc/i then GCC
       when /^lcc/i then LCC
+      when /^gcc/i then GCC
       else GenericCompiler
       end
     end
